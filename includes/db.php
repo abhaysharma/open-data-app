fbb09eb1efea9c594e2660cb60f9956cb65f7785
Abhay Sharma <abhaysharmawebd@gmail.com>
2012-03-05 12:50:58 -0500

    dsn again

--- includes/db.php
+++ includes/db.php
@@ -4,6 +4,6 @@ $user = getenv("DB_USER");
 $pass = getenv("DB_PASS");
 $dsn = stripslashes(getenv("DB_DSN"));
 
-$db = new PDO($dns, $user, $pass);
+$db = new PDO($dsn, $user, $pass);
 
 $db->exec('SET NAMES utf8');
